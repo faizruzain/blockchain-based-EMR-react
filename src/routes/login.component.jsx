104ae0cf972d143e6418b3b083658e833ab2ebd5
Faiz Ruzain <faiz.faiz8383@gmail.com>
2023-01-16 06:04:21 +0700

    tidur

--- src/routes/login.component.jsx
+++ src/routes/login.component.jsx
@@ -4,47 +4,68 @@ import {
   Form,
   Grid,
   Header,
-  Image,
   Message,
   Segment,
 } from "semantic-ui-react";
+import { redirect } from "react-router-dom";
+import axios from "axios";
+import web3 from "../ethereum/web3";
 
 class Login extends Component {
+  state = { address: "", role: "" };
+
+  async componentDidMount() {
+    const [address] = await web3.eth.getAccounts();
+    this.setState({ address: address });
+  }
+
+  verify = async () => {
+    const { address } = this.state;
+    axios
+      .get(`http://localhost:5000/verify?address=${address}`)
+      .then((res) => {
+        console.log(res.data);
+        const { role } = res.data;
+        this.setState({ role: role });
+        if (role === "doctor") {
+          return redirect("http://localhost:3000/doctor/");
+        } else if (role === "patient") {
+          return redirect("http://localhost:3000/patient/");
+        } else if (role === "unknown") {
+          this.setState({ role: role });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
+    window.ethereum.on("accountsChanged", function (accounts) {
+      window.location.reload();
+    });
+
+    const { address } = this.state;
     return (
       <Grid
         textAlign="center"
         style={{ height: "100vh" }}
         verticalAlign="middle"
       >
-        <Grid.Column style={{ maxWidth: 450 }}>
+        <Grid.Column style={{ maxWidth: 500 }}>
           <Header as="h2" color="teal" textAlign="center">
-            <Image src="/logo.png" /> Log-in to your account
+            Verify your address
           </Header>
           <Form size="large">
             <Segment stacked>
-              <Form.Input
-                fluid
-                icon="user"
-                iconPosition="left"
-                placeholder="E-mail address"
-              />
-              <Form.Input
-                fluid
-                icon="lock"
-                iconPosition="left"
-                placeholder="Password"
-                type="password"
-              />
+              <h3>{address}</h3>
 
-              <Button color="teal" fluid size="large">
-                Login
+              <Button onClick={this.verify} color="teal" fluid size="large">
+                Verify
               </Button>
             </Segment>
           </Form>
-          <Message>
-            New to us? <a href="#">Sign Up</a>
-          </Message>
+          <Message>Universitas Mercu Buana</Message>
         </Grid.Column>
       </Grid>
     );
